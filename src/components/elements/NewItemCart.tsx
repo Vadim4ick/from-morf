4710c96026591432621fc7637661241db844edd5
Mac <mac@MacBook-Pro-Mac.local>
2024-07-10 22:58:19 +0400

    add progress tab

---
+++ src/components/elements/NewItemCart.tsx
@@ -0,0 +1,73 @@
+import { cn } from "@/lib/utils";
+import { Heart } from "@/shared/icons/Heart";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+interface ItemCart {
+  id: number;
+  date: string;
+  desc: string;
+
+  images: string[];
+}
+
+const NewItemCart = ({ item }: { item: ItemCart }) => {
+  const [currentImageIdx, setCurrentImageIdx] = useState(0);
+
+  return (
+    <article className="group overflow-hidden">
+      <Link href={"/"}>
+        <div className="relative">
+          <div className="relative">
+            <Image
+              src={item.images[currentImageIdx]}
+              alt="1.png"
+              width={305}
+              height={430}
+              className="h-[430px] origin-center object-cover transition-all duration-500 group-hover:pr-[45px]"
+            />
+
+            <ul className="absolute bottom-0 left-0 mb-[10px] grid h-[2px] w-full grid-cols-3 gap-2 pl-[26px] pr-[71px] opacity-0 transition-all duration-500 group-hover:opacity-100">
+              {item.images.map((_, idx) => (
+                <li
+                  onMouseEnter={() => setCurrentImageIdx(idx)}
+                  key={idx}
+                  className={cn("w-full rounded-full", {
+                    "bg-[#9D9D9D]": idx === currentImageIdx,
+                    "bg-[#9D9D9D66]/40": idx !== currentImageIdx,
+                  })}
+                />
+              ))}
+            </ul>
+          </div>
+
+          <div className="absolute -right-[45px] top-0 h-full w-[45px] bg-darkGrayColor p-3 transition-all duration-500 group-hover:right-[0px]">
+            <div className="flex h-full flex-col justify-between">
+              <button>
+                <Heart />
+              </button>
+
+              <div className="flex flex-col items-center gap-2 border-b border-t border-[#888888] py-2 text-white">
+                <div>36</div>
+                <div>38</div>
+                <div>40</div>
+                <div>42</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-[12px] flex flex-col gap-1">
+          <p className="font-medium text-blackColor">
+            Жакет из португальского льна
+          </p>
+
+          <p className="font-bold">20 140 ₽</p>
+        </div>
+      </Link>
+    </article>
+  );
+};
+
+export { NewItemCart };
--- src/modules/HomePage/NewItems.tsx
+++ src/modules/HomePage/NewItems.tsx
@@ -2,49 +2,36 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { AdviceCart } from "../StyleAdvice/AdviceCart";
 import Image from "next/image";
-import { Heart } from "@/shared/icons/Heart";
-
-import { motion, useAnimation } from "framer-motion";
-
-const variants = {
-  hidden: { width: 0 },
-  visible: (custom: { width: number }) => ({ width: custom.width }),
-};
+import { NewItemCart } from "@/components/elements/NewItemCart";
 
 const arr = [
   {
     id: 1,
     date: "17.06.24",
     desc: "Как создать капсульный гардероб: основные принципы и идеи",
-    img: "/newItem/1.png",
+
+    images: ["/newItem/1.png", "/newItem/2.png", "/newItem/3.png"],
   },
   {
     id: 2,
     date: "17.06.24",
     desc: "Как создать капсульный гардероб: основные принципы и идеи",
-    img: "/newItem/1.png",
+
+    images: ["/newItem/1.png", "/newItem/2.png", "/newItem/3.png"],
   },
   {
     id: 3,
     date: "17.06.24",
     desc: "Как создать капсульный гардероб: основные принципы и идеи",
-    img: "/newItem/1.png",
+
+    images: ["/newItem/1.png", "/newItem/2.png", "/newItem/3.png"],
   },
-  //   {
-  //     id: 3,
-  //     date: "17.06.24",
-  //     desc: "Как создать капсульный гардероб: основные принципы и идеи",
-  //     img: "/advice/1.png",
-  //   },
 ];
 
 const NewItems = () => {
   const first = arr[0];
   const otherElements = arr.slice(1);
-
-  const controls = useAnimation();
 
   return (
     <section className="pt-16">
@@ -54,7 +41,7 @@ const NewItems = () => {
         <div className="flex gap-5">
           <Link href={"/"}>
             <Image
-              src={first.img}
+              src={first.images[0]}
               alt="1.png"
               width={628}
               height={676}
@@ -71,79 +58,10 @@ const NewItems = () => {
           </Link>
 
           <div className="flex flex-col justify-between">
-            <div className="flex h-[430px] gap-5">
+            <div className="flex gap-5">
               {otherElements.map((item) => (
-                <div key={item.id} className="group relative overflow-hidden">
-                  <Link href={"/"}>
-                    <Image
-                      src={first.img}
-                      alt="1.png"
-                      width={305}
-                      height={430}
-                      className="h-full origin-center object-cover transition-all duration-500 group-hover:pr-[45px]"
-                    />
-
-                    <div className="mt-[12px] flex flex-col gap-1">
-                      <p className="font-medium text-blackColor">
-                        Жакет из португальского льна
-                      </p>
-
-                      <p className="font-bold">20 140 ₽</p>
-                    </div>
-                  </Link>
-
-                  <motion.div className="absolute -right-[45px] top-0 h-full w-[45px] bg-darkGrayColor p-3 transition-all duration-500 group-hover:right-[0px]">
-                    <div className="flex h-full flex-col justify-between">
-                      <button>
-                        <Heart />
-                      </button>
-
-                      <div className="flex flex-col items-center gap-2 border-b border-t border-[#888888] py-2 text-white">
-                        <div>36</div>
-                        <div>38</div>
-                        <div>40</div>
-                        <div>42</div>
-                      </div>
-                    </div>
-                  </motion.div>
-                </div>
+                <NewItemCart key={item.id} item={item} />
               ))}
-              {/* {otherElements.map((item) => (
-                <div key={item.id} className="group relative">
-                  <Link href={"/"}>
-                    <Image
-                      src={first.img}
-                      alt="1.png"
-                      width={305}
-                      height={430}
-                      className="h-full object-cover"
-                    />
-
-                    <div className="mt-[12px] flex flex-col gap-1">
-                      <p className="font-medium text-blackColor">
-                        Жакет из португальского льна
-                      </p>
-
-                      <p className="font-bold">20 140 ₽</p>
-                    </div>
-                  </Link>
-
-                  <div className="absolute right-0 top-0 hidden h-full w-[45px] bg-darkGrayColor p-3 group-hover:block">
-                    <div className="flex h-full flex-col justify-between">
-                      <button>
-                        <Heart />
-                      </button>
-
-                      <div className="flex flex-col items-center gap-2 border-b border-t border-[#888888] py-2 text-white">
-                        <div>36</div>
-                        <div>38</div>
-                        <div>40</div>
-                        <div>42</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))} */}
             </div>
 
             <Button className="mb-12 ml-auto w-fit" variant={"default"}>
