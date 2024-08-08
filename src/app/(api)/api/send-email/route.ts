d33a6c6b91ca8fa867f367f4636e30711126df09
Mac <mac@MacBook-Pro-Mac.local>
2024-08-08 12:07:29 +0400

    createActivate token

--- src/app/(api)/api/send-email/route.ts
+++ src/app/(api)/api/send-email/route.ts
@@ -5,8 +5,6 @@ import nodemailer from "nodemailer";
 export async function POST(request: Request) {
   const formData = await request.json();
 
-  console.log("Start");
-
   const email = formData.email;
   const password = formData.password;
 
@@ -20,35 +18,27 @@ export async function POST(request: Request) {
     },
   });
 
-  console.log("transporter", transporter);
+  try {
+    const { token, activationCode } = await createActivationToken(
+      email,
+      password,
+    );
 
-  return NextResponse.json({ status: 200, body: formData });
+    // await transporter.sendMail({
+    //   from: "Message bot", // sender address
+    //   to: email, // list of receivers
+    //   subject: `Сообщение от From-Morf`, // Subject line
+    //   // text: JSON.stringify(message), // plain text body
+    //   html: `
+    //    Код будет действителен в течении 5 минут.
 
-  // try {
-  //   const { token, activationCode } = await createActivationToken(
-  //     email,
-  //     password,
-  //   );
+    //    Code ${activationCode}
+    //   `, // html body
+    // });
 
-  //   console.log("activationCode", activationCode);
-
-  //   await transporter.sendMail({
-  //     from: "Message bot", // sender address
-  //     to: email, // list of receivers
-  //     subject: `Сообщение от From-Morf`, // Subject line
-  //     // text: JSON.stringify(message), // plain text body
-  //     html: `
-  //      Код будет действителен в течении 5 минут.
-
-  //      Code ${activationCode}
-  //     `, // html body
-  //   });
-
-  //   console.log("End");
-
-  //   return NextResponse.json({ status: 200, activationToken: token });
-  // } catch (err) {
-  //   console.log(err);
-  //   return NextResponse.json({ status: 500 });
-  // }
+    return NextResponse.json({ status: 200, activationToken: token });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ status: 500 });
+  }
 }
