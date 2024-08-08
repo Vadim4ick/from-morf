797be06c1c3f9f3860b283edf6f1215579f2b0b6
Mac <mac@MacBook-Pro-Mac.local>
2024-08-08 10:23:19 +0400

    тест ссылка на гл стр

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import { gql } from "@/graphql/client";
 import HomePage from "@/pages/HomePage";
+import { useEffect } from "react";
 
-export default async function Home() {
-  const { homePage } = await gql.GetHomePage();
-  const { styleTips } = await gql.GetLastTwoStyleTips();
-  const { lookBock } = await gql.GetHomePageLookBook();
+function test() {
+  fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/items/goods`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
 
-  return (
-    <HomePage styleTips={styleTips} homePage={homePage} lookBock={lookBock} />
-  );
+export default function Home() {
+  // const { homePage } = await gql.GetHomePage();
+  // const { styleTips } = await gql.GetLastTwoStyleTips();
+  // const { lookBock } = await gql.GetHomePageLookBook();
+
+  useEffect(() => {
+    test();
+  }, []);
+
+  // return (
+  //   <HomePage styleTips={styleTips} homePage={homePage} lookBock={lookBock} />
+  // );
+
+  return <div>123</div>;
 }
