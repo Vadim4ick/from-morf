5b6b3a8ffb96585e84b05e20d9db3c3f88454577
Mac <mac@MacBook-Pro-Mac.local>
2024-08-08 10:27:35 +0400

    fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,12 @@
-"use client";
-
 import { gql } from "@/graphql/client";
 import HomePage from "@/pages/HomePage";
-import { useEffect } from "react";
 
-function test() {
-  fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/items/goods`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-}
+export default async function Home() {
+  const { homePage } = await gql.GetHomePage();
+  const { styleTips } = await gql.GetLastTwoStyleTips();
+  const { lookBock } = await gql.GetHomePageLookBook();
 
-export default function Home() {
-  // const { homePage } = await gql.GetHomePage();
-  // const { styleTips } = await gql.GetLastTwoStyleTips();
-  // const { lookBock } = await gql.GetHomePageLookBook();
-
-  useEffect(() => {
-    test();
-  }, []);
-
-  // return (
-  //   <HomePage styleTips={styleTips} homePage={homePage} lookBock={lookBock} />
-  // );
-
-  return <div>123</div>;
+  return (
+    <HomePage styleTips={styleTips} homePage={homePage} lookBock={lookBock} />
+  );
 }
