93649fc637dc2f385c98db474456ce5752a8125c
Mac <mac@MacBook-Pro-Mac.local>
2024-08-12 14:18:49 +0400

    fix new bags

--- src/app/style-tips/page.tsx
+++ src/app/style-tips/page.tsx
@@ -1,8 +1,13 @@
 import { gql } from "@/graphql/client";
-import { StyleTipsPage } from "@/pages/StyleTipsPage";
+import StyleTipsPage from "@/pages/StyleTipsPage";
+import { notFound } from "next/navigation";
 
 const Page = async () => {
   const { styleTips } = await gql.GetTipsPage();
+
+  if (!styleTips) {
+    return notFound();
+  }
 
   return <StyleTipsPage styleTips={styleTips} />;
 };
--- src/pages/StyleTipsPage.tsx
+++ src/pages/StyleTipsPage.tsx
@@ -27,15 +27,16 @@ const StyleTipsPage = ({
         <h1 className="heading pb-8 max-tabletBig:pb-6">Советы по стилю</h1>
 
         <div className="grid max-w-[962px] grid-cols-2 gap-x-[20px] gap-y-[48px] max-mobile:grid-cols-1 max-mobile:justify-items-center max-mobile:gap-y-9">
-          {styleTips.map((el) => (
-            <article key={el.id} className="max-w-[466px]">
-              <AdviceCart item={el} />
-            </article>
-          ))}
+          {styleTips &&
+            styleTips.map((el) => (
+              <article key={el.id} className="max-w-[466px]">
+                <AdviceCart item={el} />
+              </article>
+            ))}
         </div>
       </div>
     </motion.section>
   );
 };
 
-export { StyleTipsPage };
+export default StyleTipsPage;
